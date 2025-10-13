e7825ac1164f860f9e8fe80d72a7030a6cdc618d
Jeyavarma <Jeyavarma@email.com>
2025-10-13 18:28:51 +0530

    Fix EmailJS template parameters
    - Update service ID to LostandFound_otp
    - Change template parameters to match EmailJS config:
      - to_email -> email
      - otp_code -> passcode
      - user_name -> removed
      - Added time parameter (10 minutes)
    - Updated both forgot-password and test-email pages

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -46,9 +46,9 @@ export default function ForgotPasswordPage() {
             process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
             process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
             {
-              to_email: email,
-              otp_code: data.otp,
-              user_name: email.split('@')[0]
+              email: email,
+              passcode: data.otp,
+              time: 10
             },
             process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
           )
